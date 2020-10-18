59b173e2a7bc7ea40ab1bc14e0b07874eefdb2cd
Nihad <ngazic@gmail.com>
2020-10-18 02:41:30 +0200

    sign in component initial tests

---
+++ tests/unit/components/signin.spec.ts
@@ -0,0 +1,21 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+
+describe('SignIn.vue', () => {
+
+  it('renders correclty', () => {
+    
+  });
+
+  it('dispatches "login" action when submit button is clicked', () => {
+    
+  });
+
+  it('router called on successful login ', () => {
+    
+  });
+
+  it('resets form data when reset button is clicked', () => {
+    
+  })
+  
+});
