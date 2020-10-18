4d7c0a9780fc8a2a0f6ed178d48a9c8eafbe8b4f
Nihad <ngazic@gmail.com>
2020-10-18 13:48:27 +0200

    added tests for SingIn.vue component

--- tests/unit/components/signin.spec.ts
+++ tests/unit/components/signin.spec.ts
@@ -1,21 +1,87 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { shallowMount, createLocalVue, Wrapper, createWrapper } from '@vue/test-utils'
+import Vuex, { ActionTree, Store, StoreOptions } from 'vuex'
+import flushPromises from 'flush-promises'
+import VueRouter from 'vue-router'
+import SignIn from '@/components/Auth/SignIn.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(VueRouter)
+const router = new VueRouter()
+jest.spyOn(window, 'alert').mockImplementation(() => { });
 
 describe('SignIn.vue', () => {
 
+  const MockFormData = {
+    user: 'John Doe',
+    password: 'secret1234'
+  }
+
+  let user: Wrapper<SignIn>
+  let password: Wrapper<SignIn>
+  let form: Wrapper<SignIn>
+
+  function createConfig(overrides: StoreOptions<any>): Wrapper<SignIn> {
+    const store = new Vuex.Store(overrides)
+
+    const wrapper = shallowMount(SignIn, {
+      localVue,
+      store,
+      router
+    })
+    user = wrapper.find('#user')
+    password = wrapper.find('#password')
+    form = wrapper.find('form')
+    user.setValue(MockFormData.user)
+    password.setValue(MockFormData.password)
+    return wrapper
+  }
+
   it('renders correclty', () => {
-    
+    const wrapper = shallowMount(SignIn)
+    expect(wrapper).toMatchSnapshot()
   });
 
-  it('dispatches "login" action when submit button is clicked', () => {
-    
+  it('dispatches "login" action when submit button is clicked', async () => {
+    const actions = {
+      login: jest.fn()
+    }
+
+
+
+    const wrapper = createConfig({ actions });
+    await form.trigger('submit.prevent')
+    const spy = jest.spyOn(actions, 'login')
+    expect(actions.login).toHaveBeenCalled()
+
   });
 
-  it('router called on successful login ', () => {
-    
+  it('router called on successful login ', async () => {
+    const config = {
+      actions: {
+        login: jest.fn().mockResolvedValue({})
+      }
+      , getters: {
+        idUser: (): string => '12345'
+      }
+    }
+
+    const wrapper = createConfig(config);
+    const spy = jest.spyOn(wrapper.vm.$router, 'push')
+    await form.trigger('submit.prevent')
+    expect(spy).toBeCalled()
   });
 
-  it('resets form data when reset button is clicked', () => {
-    
-  })
-  
+
+  it('should show allert message on the REST API error', async () => {
+    const actions = {
+      login: jest.fn().mockRejectedValue({ err: 'error message' })
+    }
+    const wrapper = createConfig({ actions })
+    const spy = jest.spyOn(window, 'alert')
+    await form.trigger('submit.prevent')
+    expect(spy).toHaveBeenCalledWith({ err: 'error message' })
+  });
+
+
 });
