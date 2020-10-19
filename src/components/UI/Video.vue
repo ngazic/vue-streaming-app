1363293314ed11ee22d59bc3d9aeadf5bbc7e257
Nihad <ngazic@gmail.com>
2020-10-19 07:11:59 +0200

    updated store and Video.vue to work with store data

---
+++ src/components/UI/Video.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col col-sm-10 ">
+      <h1>{{ getCurrentVideo().title }}</h1>
+      <video vjs-big-play-centered :src="getCurrentVideo().url" muted controls class="video-js" data-setup="{}"></video>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import {
+  mapGetters
+} from 'vuex'
+export default {
+  methods: {
+    ...mapGetters(["getCurrentVideo"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.video-js {
+  width: 100%;
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import {Video} from '@/mocks/video-data'
 Vue.use(Vuex)
+
+const initialVideo: Video = {
+  id: -1,
+  poster: '',
+  price: 0,
+  title: '',
+  url: ''
+}
 
 export default new Vuex.Store({
   state: {
-    idToken: ''
+    idToken: '',
+    currentlyPlayingVideo: initialVideo
   },
   getters: {
-    isAuthenticated(state): boolean{
-      return state.idToken !== ''
+    isAuthenticated(): boolean{
+      return true;
+      // return state.idToken !== ''
+    },
+    getCurrentVideo(state): Video {
+      return state.currentlyPlayingVideo
     }
   },
   mutations: {
+    currentVideoMutation(state, video: Video): void {
+     state.currentlyPlayingVideo = video
+    },
   },
   actions: {
+    paymentAction({ commit }, video) {
+      commit('currentVideoMutation', video)
+    }
   },
   modules: {
   }
