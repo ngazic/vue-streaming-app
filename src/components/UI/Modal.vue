25c32c25b004cba662ad2ce22265761f36180e35
Nihad <ngazic@gmail.com>
2020-10-19 05:09:15 +0200

    Modal.vue component added

---
+++ src/components/UI/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+<!-- Modal -->
+<div class="modal-dialog modal-dialog-centered">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="cancel">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      ...
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="cancel">
+        Close
+      </button>
+      <button type="button" class="btn btn-primary" @click="agreed">Understood</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  methods: {
+    agreed(): void {
+      this.$emit('handlePayment', true)
+    },
+    cancel(): void {
+      this.$emit('handlePayment', false)
+    }
+  }
+};
+</script>
