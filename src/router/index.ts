c9fa0acfb56157e8a0cc11732ed1fc4ed17cd8ab
Nihad <ngazic@gmail.com>
2020-10-19 07:26:20 +0200

    authentification added for /watch route

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,22 +4,34 @@ import Home from '../views/Home.vue'
 import store from '../store'
 import SignIn from '@/components/Auth/SignIn.vue'
 import SignUp from '@/components/Auth/SignUp.vue'
+import Video from '@/components/UI/Video.vue'
 
 Vue.use(VueRouter)
+
+
+function authentification(to, from, next): void {
+  if (store.getters.isAuthenticated) {
+    next()
+  } else {
+    next('/signin')
+  }
+}
 
 export const routes: Array<RouteConfig> = [
   { path: '/signup', component: SignUp },
   { path: '/signin', component: SignIn },
   {
+    path: '/watch', component: Video,
+    beforeEnter(to, from, next) {
+      authentification(to, from, next)
+    }
+  },
+  {
     path: '/',
     name: 'Home',
     component: Home,
     beforeEnter(to, from, next) {
-      if (store.state.idToken) {
-        next()
-      } else {
-        next('/signin')
-      }
+      authentification(to, from, next)
     }
   },
   {
