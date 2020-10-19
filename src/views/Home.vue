fd50c658961a1035d173ad0bc1129660e921f38e
Nihad <ngazic@gmail.com>
2020-10-19 05:07:02 +0200

    Home.vue added functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div class="home">
+  <div class="container-fluid">
+    <div v-if="!showModal" class="row">
+      <div class="col-12 col-sm-6 col-md-4 col-lg-2" v-for="video in videos" :key="video.id">
+        <div class="card" @click="click(video)">
+          <div class="card-body">
+            <img :src="video.poster" class="card-img-top" />
+            <h5 class="card-title">{{ video.title }}</h5>
+            <p>Price {{ video.price }}$</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <modal v-if="showModal" @handlePayment="payment" />
   </div>
+</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+import Modal from "@/components/UI/Modal.vue";
+import {
+  Video,
+  getVideos
+} from "../mocks/video-data";
 
+const initialVideoList: Video[] = [];
+let initialVideo: Video;
 export default Vue.extend({
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld,
+    Modal
   },
+  async created() {
+    this.videos = await getVideos();
+  },
+  data() {
+    return {
+      videos: initialVideoList,
+      showModal: false,
+      paymentVerified: false,
+      currentVideo: initialVideo
+    };
+  },
+  methods: {
+    payment(data: boolean): void {
+      console.log(data);
+      if (data) {
+        this.$store.dispatch("payment", this.currentVideo);
+        this.$store;
+        this.$router.push("/watch");
+      }
+      this.showModal = false;
+    },
+    click(video: Video): void {
+      this.showModal = true;
+      this.currentVideo = video;
+      console.log(video);
+    }
+  }
 });
 </script>
