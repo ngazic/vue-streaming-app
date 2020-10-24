fee93bffcc4e52fcf4c5ad1a0c94277bba36a884
Nihad <ngazic@gmail.com>
2020-10-25 00:26:28 +0200

    vide.spec.ts tests updated

--- tests/unit/components/video.spec.ts
+++ tests/unit/components/video.spec.ts
@@ -1,11 +1,35 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
-import Video from '@/components/UI/Video.vue'
+import VideoComponent from '@/components/UI/Video.vue'
+import { Video } from '@/mocks/video-data.types'
 
 describe('Video.vue', () => {
-
-  it('renders correct video from Vuex', () => {
-    expect(true).toBe(false)
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  const getters = {
+    getCurrentVideo: (): Video => currentVideo
+  }
+  const store = new Vuex.Store({
+    getters
+  })
+  const currentVideo: Video = {
+    id: 100,
+    price: 200,
+    title: "test video",
+    poster: "test",
+    url: "test.test"
+  }
+  const wrapper = shallowMount(VideoComponent, {
+    localVue,
+    store
+  })
+  it('renders current video\'s title from Vuex', () => {
+    const title = wrapper.find('.container h1')
+    expect(title.text()).toBe(getters.getCurrentVideo().title)
+  });
+  it('get current video\'s url from Vuex', () => {
+    const url = wrapper.find('.container video').attributes('src')
+    expect(url).toBe(getters.getCurrentVideo().url)
   });
 })
